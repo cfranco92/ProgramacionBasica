07128210e35c20f0c1bb0408677ba27478037c49
cfrancobedoya <cfrancobedoya@gmail.com>
2017-01-12 23:44:01 -0500

    VISTO HASTA EL MOMENTO
    Se obtienen datos del usuario, se separa el javascript del html, se
    realiza un pequeño proyecto para aprender a usar variables.

---
+++ separacionJavascript/codigo.js
@@ -0,0 +1,10 @@
+alert("Sí funciona");
+
+<!-- Write escribe en el momento de uso -->
+  document.write("Hola estudiante");
+<!-- Window es toda la ventana, variable del objeto-->
+  var ruta = window.location; <!--location es un objeto-->
+  console.log("Esto es una caca secreta");
+  console.log(ruta);
+  document.write("Estas en: " + ruta);
+  document.write("Estas en: " + ruta.origin); <!--se tome el origen del documento-->
