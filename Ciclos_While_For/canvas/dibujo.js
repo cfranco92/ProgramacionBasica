d92d8794c0119478a0311a46a5460750f63812f4
cfrancobedoya <cfrancobedoya@gmail.com>
2017-01-18 13:22:15 -0500

    CICLOS FOR Y WHILE
    Dibujando en canvas usando ciclos.

---
+++ Ciclos_While_For/canvas/dibujo.js
@@ -0,0 +1,56 @@
+
+var d = document.getElementById("dibujito");  <!--Aqui guardamos el canvas-->
+<!--Lienzo en 2D-->
+var lienzo = d.getContext("2d"); <!--Área donde va a dibujar-->
+console.log(lienzo);
+
+function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal)
+{
+  lienzo.beginPath();
+  lienzo.strokeStyle = color;
+  lienzo.moveTo(xinicial,yinicial);
+  lienzo.lineTo(xfinal,yfinal);
+  lienzo.stroke();
+  lienzo.closePath();
+}
+
+/* SE LLAMA LA FUNCIÓN */
+dibujarLinea("#AAF", 0, 0, 10, 300);
+
+var lineas = 30;
+var l = 0;
+var yi, xf;
+var colorcito = "#FAA"
+
+/*
+while( l < lineas){
+  yi = 10 * l;
+  xf = 10 * (l+1);
+  dibujarLinea(colorcito, 0, yi, xf, 300);
+  console.log("Linea " + l);
+  l++; //l = l+1;
+}*/
+
+colorcito = "#AAF";
+
+for(l = 0; l < 30; l++){
+  yi = 10 * l;
+  xf = 10 * (l+1);
+  dibujarLinea(colorcito, 0, yi, xf, 300);
+  console.log("Linea " + l);
+}
+
+dibujarLinea(colorcito,1,1,1,299);  /*Esto se hace porque en 0 se dibuja la mitad del pixel*/
+dibujarLinea(colorcito,1,299,299,299);
+
+
+colorcito2 = "#e02239";
+for(l = 0; l < 30; l++){
+  yi = 10 * l;
+  xf = 10 * (l+1);
+  dibujarLinea(colorcito2, yi, 0, 300, xf);
+  console.log("Linea " + l);
+}
+
+dibujarLinea("rgb(245, 95, 225)",1,1,299,1);
+dibujarLinea("rgb(245, 95, 225)",299,1,299,299);
