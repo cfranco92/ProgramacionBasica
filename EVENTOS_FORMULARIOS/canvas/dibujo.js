56c3ba441e96e1bae792394a9580f7f20fa9064d
cfrancobedoya <cfrancobedoya@gmail.com>
2017-01-20 00:10:36 -0500

    EVENTOS Y FORMULARIOS
    Se toma el texto de un textBox y se toma el evento al hacer click en un
    botón.

--- EVENTOS_FORMULARIOS/canvas/dibujo.js
+++ EVENTOS_FORMULARIOS/canvas/dibujo.js
@@ -1,7 +1,13 @@
+<!--SE TRAEN EL TEXTO Y EL BOTÓN DEL HTML-->
+var texto = document.getElementById("texto_lineas");
+var boton = document.getElementById("botoncito");
+<!--EVENT LISTENER-->
+<!--se para una referencia de la función para ejecutar en el envento. -->
+boton.addEventListener("click", dibujoPorClick);
 
-var d = document.getElementById("dibujito");  <!--Aqui guardamos el canvas-->
-<!--Lienzo en 2D-->
-var lienzo = d.getContext("2d"); <!--Área donde va a dibujar-->
+var d = document.getElementById("dibujito");
+var ancho = d.width;
+var lienzo = d.getContext("2d");
 console.log(lienzo);
 
 function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal)
@@ -14,32 +20,29 @@ function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal)
   lienzo.closePath();
 }
 
-/* SE LLAMA LA FUNCIÓN */
-dibujarLinea("#AAF", 0, 0, 10, 300);
 
-var lineas = 30;
-var l = 0;
-var yi, xf;
-var colorcito = "#AAF"
+<!--FUNCIÓN EVENTOS-->
+function dibujoPorClick()
+{
+  var x = texto.value; <!--value trae el contenido del objeto input-->
+  var xInt = parseInt(texto.value);
+  dibujarLinea("#AAF", 0, 0, 10, 300);
 
-for(l = 0; l < 30; l++){
-  yi = 10 * l;
-  xf = 10 * (l+1);
-  dibujarLinea(colorcito, 0, yi, xf, 300);
-  console.log("Linea " + l);
+  var lineas = parseInt(texto.value);
+  var l = 0;
+  var yi, xf;
+  var colorcito = "#AAF"
+  var espacio = ancho/lineas;
+
+  for(l = 0; l < lineas; l++){
+    yi = espacio * l;
+    xf = espacio * (l+1);
+    dibujarLinea(colorcito, 0, yi, xf, 300);
+    console.log("Linea " + l);
+  }
+
+  dibujarLinea(colorcito,1,1,1,299);  /*Esto se hace porque en 0 se dibuja la mitad del pixel*/
+  dibujarLinea(colorcito,1,299,299,299);
+  console.log("Cristian es genial: " + texto.value);
+  console.log(xInt);
 }
-
-dibujarLinea(colorcito,1,1,1,299);  /*Esto se hace porque en 0 se dibuja la mitad del pixel*/
-dibujarLinea(colorcito,1,299,299,299);
-
-
-colorcito2 = "#e02239";
-for(l = 0; l < 30; l++){
-  yi = 10 * l;
-  xf = 10 * (l+1);
-  dibujarLinea(colorcito2, yi, 0, 300, xf);
-  console.log("Linea " + l);
-}
-
-dibujarLinea("rgb(245, 95, 225)",1,1,299,1);
-dibujarLinea("rgb(245, 95, 225)",299,1,299,299);
