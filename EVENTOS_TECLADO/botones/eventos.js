c9ca7c4c09f2abd61ae781f064d1d46ce6dea405
cfrancobedoya <cfrancobedoya@gmail.com>
2017-01-21 20:10:03 -0500

    EVENTOS TECLADO
    Se reciben eventos del teclado, y se utiliza switch
    para tomas las 4 teclas principales de movimiento. Arriba, izquierda,
    derecha y abajo.

--- EVENTOS_TECLADO/botones/eventos.js
+++ EVENTOS_TECLADO/botones/eventos.js
@@ -1,8 +1,35 @@
-  <!--EVENTO AL OPRIMIR UNA TECLA-->
-document.addEventListener("keydown", dibujarTeclado);
+var teclas = {
+  UP: 38,
+  DOWN: 40,
+  LEFT: 37,
+  RIGHT: 39
+};
 
-<!--en detalles quedan guardados todos los detalles del evento-->
-function dibujarTeclado(detalles){
-  console.log("Tecla oprimida");
-  console.log(detalles);
+console.log(teclas);
+
+  <!--EVENTO AL OPRIMIR UNA TECLA-->
+/*document.addEventListener("keydown", dibujarTeclado);*/
+document.addEventListener("keyup", dibujarTeclado);
+
+<!--En ventos quedan guardados todos los detalles del evento-->
+function dibujarTeclado(evento){
+
+  switch (evento.keyCode)
+  {
+    case teclas.UP:
+      console.log("Arriba");
+    break;
+    case teclas.DOWN:
+      console.log("Abajo");
+    break;
+    case teclas.LEFT:
+      console.log("Izquierda");
+    break;
+    case teclas.RIGHT:
+      console.log("Derecha");
+    break;
+    default:
+      console.log("Otra tecla");
+    break;
+  }
 }
